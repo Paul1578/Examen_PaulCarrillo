382cd26acdd7f8d165ff2bce57ca61cc7edb4c1d
angel0314ist <alc.quishpe@yavirac.edu.ec>
2025-10-23 11:37:50 -0500

    Arreglo de los test y aumento de logica para segundo test

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hola mundo desde node.Js este es un app para el examen de DevOps!');
 });
 
+
+app.get('/data', (req, res) => {
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Error leyendo el archivo');
+    }
+    res.json(JSON.parse(data)); 
+  });
+});
+
+// Levanta el servidor
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
+
+module.exports = app;
--- app.test.js
+++ app.test.js
@@ -1,9 +1,19 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('./app'); 
 
 describe('GET /', () => {
-  it('responds with Hello, World!', async () => {
+  it('responds with Hola mundo desde node.Js este es un app para el examen de DevOps!', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toBe('Hello, World!');
+    expect(response.text).toBe('Hola mundo desde node.Js este es un app para el examen de DevOps!');
+  });
+});
+
+describe('GET /data', () => {
+  it('responds with the data from data.json', async () => {
+    const response = await request(app).get('/data');
+    const data = JSON.parse(response.text); 
+    expect(response.status).toBe(200); 
+    expect(data.name).toBe('Examen Proyecto');  
+    expect(data.description).toBe('Aplicación de ejemplo para pruebas.'); 
   });
 });
